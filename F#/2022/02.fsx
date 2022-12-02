9c370c29b6a5a3fb9c9163431082d55ac03fa433
Dimension4 <Dimension4@users.noreply.github.com>
2022-12-02 15:26:55 +0100

    F# day 2

---
+++ F#/2022/02.fsx
@@ -0,0 +1,35 @@
+
+open System
+open System.IO
+
+type Shape =
+    | Rock = 0
+    | Paper = 1
+    | Scissors = 2
+
+let parseShape = function
+    | "A" | "X" -> Shape.Rock
+    | "B" | "Y" -> Shape.Paper
+    | "C" | "Z" -> Shape.Scissors
+
+let arrayToTuple [| a; b |] = a, b
+let split (s: string) = s.Split(' ')
+
+let score (a: Shape, b: Shape) = ((4 + int b - int a) % 3) * 3 + 1 + int b
+
+let input = fsi.CommandLineArgs[1] |> File.ReadAllLines |> Array.map split
+
+input
+|> Seq.map (Array.map parseShape >> arrayToTuple >> score)
+|> Seq.sum
+|> printfn "part 1: %i"
+
+// part 2
+
+let parseOutcome outcome shape =
+    shape, (int shape + match outcome with | "X" -> 2 | "Y" -> 0 | "Z" -> 1) % 3 |> enum<Shape>
+
+input
+|> Seq.map (fun [| a; b |] -> a |> parseShape |> parseOutcome b |> score)
+|> Seq.sum
+|> printfn "part 2: %i"
