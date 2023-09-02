d744cf6e559af82f20cd4d0f9bd15f10eeda428f
Dimension4 <Dimension4@users.noreply.github.com>
2023-09-03 08:49:09 +0900

    Add script to download input files

---
+++ F#/2022/DownloadInputs.fsx
@@ -0,0 +1,24 @@
+﻿open System.IO
+open System.Net.Http
+
+let sessionId = fsi.CommandLineArgs[1]
+let year = 2022
+
+task {
+    use client = new HttpClient()
+    client.DefaultRequestHeaders.Add("Cookie", $"session={sessionId}")
+    for i in 1..24 do
+        try
+            let fileName = Path.Combine(__SOURCE_DIRECTORY__, $"%02d{i}.txt")
+            if File.Exists fileName then
+                printfn $"'{fileName}' already exists, skipping."
+            else
+                let! response = client.GetStringAsync $"https://adventofcode.com/{year}/day/{i}/input"
+                do! File.WriteAllTextAsync(fileName, response)
+                printfn $"Saved '{fileName}'"
+        with _ -> eprintfn $"Failed to fetch input for day {i}."
+}
+|> Async.AwaitTask
+|> Async.RunSynchronously
+
+
