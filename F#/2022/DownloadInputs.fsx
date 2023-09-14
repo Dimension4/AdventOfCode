a592f2a688b03bc61329a1cc086643b47366645a
Dimension4 <Dimension4@users.noreply.github.com>
2023-09-14 23:58:09 +0200

    More flexible download script

--- F#/2022/DownloadInputs.fsx
+++ F#/2022/DownloadInputs.fsx
@@ -2,14 +2,15 @@
 open System.Net.Http
 
 let sessionId = fsi.CommandLineArgs[1]
-let year = 2022
+let year = Array.tryItem 2 fsi.CommandLineArgs |> Option.map int |> Option.defaultValue 2022
+let dest = Array.tryItem 3 fsi.CommandLineArgs |> Option.defaultValue __SOURCE_DIRECTORY__
 
 task {
     use client = new HttpClient()
     client.DefaultRequestHeaders.Add("Cookie", $"session={sessionId}")
     for i in 1..24 do
         try
-            let fileName = Path.Combine(__SOURCE_DIRECTORY__, $"%02d{i}.txt")
+            let fileName = Path.Combine(dest, $"%02d{i}.txt")
             if File.Exists fileName then
                 printfn $"'{fileName}' already exists, skipping."
             else
